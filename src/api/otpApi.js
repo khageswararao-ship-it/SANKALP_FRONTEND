c3e518db2c71122892974b0b91e068c72f3c7464
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:27:28 +0530

    Update otpApi.js

--- src/api/otpApi.js
+++ src/api/otpApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://10.205.165.151:8080/api/otp";
+const API = "https://sankalp-backend-r2sj.onrender.com";
 
 export const sendOtp = (employeeId) => {
   return axios.post(`${API}/send`, {
