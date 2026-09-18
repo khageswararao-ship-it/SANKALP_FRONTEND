1b0f4b867a34c92418adf31138ec7ba10b8f4957
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:24:22 +0530

    Update leaveApi.js

--- src/api/leaveApi.js
+++ src/api/leaveApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.205.165.151:8080/leave";
+const API_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 export const getLeave = () => axios.get(API_URL);
 
