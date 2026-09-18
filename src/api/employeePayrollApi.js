186a1b2d560652bcba57538d7e79d6e4d1b6b2a9
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:20:41 +0530

    Update employeePayrollApi.js

--- src/api/employeePayrollApi.js
+++ src/api/employeePayrollApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://10.205.165.151:8080/api/payroll",
+  baseURL: "https://sankalp-backend-r2sj.onrender.com";
 });
 
 export const getPayroll = (employeeId) =>
