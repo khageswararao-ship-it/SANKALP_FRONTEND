c2189df4b16b01734970aa0ec3bcc900c126e6ef
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:28:18 +0530

    Update payrollApi.js

--- src/api/payrollApi.js
+++ src/api/payrollApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.205.165.151:8080/api/payroll";
+const API_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 export const getPayroll = () => axios.get(API_URL);
 
