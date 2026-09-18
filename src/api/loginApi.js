8b578ae620ed2f9f8a4cc81aa57d9024db78433a
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:25:30 +0530

    Update loginApi.js

--- src/api/loginApi.js
+++ src/api/loginApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.205.165.151:8080/api/login";
+const API_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 export const loginUser = async (loginData) => {
   const response = await axios.post(
