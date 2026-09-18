1e2968a0e8624c99416202ac850426196c3c7fbc
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:21:42 +0530

    Update employeeSettingsApi.js

--- src/api/employeeSettingsApi.js
+++ src/api/employeeSettingsApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.205.165.151:8080/api/employee/settings";
+const API_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 // Get Employee Settings
 export const getEmployeeSettings = async (employeeId) => {
