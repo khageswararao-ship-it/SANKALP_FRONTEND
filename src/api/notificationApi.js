97c267145af827164f918493d212f772124d33fd
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:26:48 +0530

    Update notificationApi.js

--- src/api/notificationApi.js
+++ src/api/notificationApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.205.165.151:8080/api/notifications";
+const API_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 // ========================
 // Admin APIs
