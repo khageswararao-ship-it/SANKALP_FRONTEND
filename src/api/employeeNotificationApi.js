545f45e8c43a688952c5646257b5345ecfe3e07c
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:20:22 +0530

    Update employeeNotificationApi.js

--- src/api/employeeNotificationApi.js
+++ src/api/employeeNotificationApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://10.205.165.151:8080/api/employee/notifications";
+const BASE_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 // Get all notifications
 export const getNotifications = () => {
