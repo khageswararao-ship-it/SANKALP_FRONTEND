39151aecf09637b3867a7e8bd7fa70ca5bed4557
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:21:05 +0530

    Update employeeProfileApi.js

--- src/api/employeeProfileApi.js
+++ src/api/employeeProfileApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://10.205.165.151:8080/api/employee/profile";
+const BASE_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 // Get Profile
 export const getEmployeeProfile = (employeeId) => {
