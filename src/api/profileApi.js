fc5198c78f81d1da4c997278b03663826eb6ce7a
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:28:33 +0530

    Update profileApi.js

--- src/api/profileApi.js
+++ src/api/profileApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.205.165.151:8080/api/profile";
+const API_URL = "https://sankalp-backend-r2sj.onrender.com";
 
 export const getProfile = () => axios.get(API_URL);
 
