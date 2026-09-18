b09b2a509e8785c66c48e7a7339738d960fdac97
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:29:15 +0530

    Update settingsApi.js

--- src/api/settingsApi.js
+++ src/api/settingsApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.205.165.151:8080/api/settings";
-
+const API_URL = "https://sankalp-backend-r2sj.onrender.com";
 export const getSettings = () => axios.get(API_URL);
 
 export const updateSettings = (settings) =>
