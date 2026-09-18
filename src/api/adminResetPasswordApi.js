e3a4c477903014a0a00d0caea72357721e2e13ae
Khageswararao <khageswararaor@gmail.com>
2026-09-18 20:17:45 +0530

    Update adminResetPasswordApi.js

--- src/api/adminResetPasswordApi.js
+++ src/api/adminResetPasswordApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://10.205.165.151:8080/api/admin";
-
+const BASE_URL = "https://sankalp-backend-r2sj.onrender.com";
 export const adminResetPassword = async (token, password) => {
 
     const response = await axios.post(
